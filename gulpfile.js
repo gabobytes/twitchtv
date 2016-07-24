326f6c86d710ddea81db0e99fa9ad8d33c60c40b
Gabriel Melo <gabrielmeloarteaga@gmail.com>
2016-07-24 18:55:03 -0500

    version 1.0.0

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+/* Dependencies */
+var gulp = require('gulp'),
+	uglify = require('gulp-uglify'),
+	concat = require('gulp-concat'),
+	livereload = require('gulp-livereload'),
+	uglifycss = require('gulp-uglifycss');
+
+//Script tasks
+gulp.task('scripts',function(){
+	gulp.src('js/source/*.js')
+		.pipe(uglify({mangle: false}))
+		.pipe(concat('app.js'))
+		.pipe(gulp.dest('js/build/'))
+		.pipe(livereload());
+});	
+
+gulp.task('css',function(){
+	gulp.src('css/source/*.css')
+	.pipe(uglifycss({mangle: false}))
+	.pipe(concat('styles.css'))
+	.pipe(gulp.dest('css/build/'))
+	.pipe(livereload());
+});
+
+gulp.task('html',function(){
+	gulp.src('*.html')
+	.pipe(livereload());
+});
+
+//Watch task
+gulp.task('watch',function(){
+	livereload.listen();
+
+	gulp.watch('js/source/*.js',['scripts']);
+	gulp.watch('css/source/*.css',['css']);
+	gulp.watch('*.html',['html']);
+});
+
+//default gulp task
+gulp.task('default',['scripts','css','watch'])
---
+++ js/source/app.js
@@ -0,0 +1,45 @@
+var app = angular.module('tvApp',[]);
+app.controller('tvCtrl', function($scope,$http){
+  
+  //values to show or hide online or offline users
+  $scope.onlineUsers = true;
+  $scope.offlineUsers = true;
+  
+  var urlApi = "https://api.twitch.tv/kraken/streams/";
+
+  //array of users
+  var users = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","comster404","OgamingSC2","thomasballinger","cretetion", "noobs2ninjas","beohoff", "sheevergaming","brunofin","ESL_SC2"];
+  
+  //arrays to store online and offline users
+  $scope.online = [];
+  $scope.offline = [];
+  
+  //this is just for practicing angular xD
+  $scope.titleApp = "Twitch.Tv Streamers";
+  
+  //query every user status with urlApi
+  angular.forEach(users,function(user){    
+    $http.get(urlApi + user).success(function(data){
+    
+      if(data.stream){//if user is online
+          $scope.online.push(data); 
+        }else{// else, user offline and check again API to get data of user offline          
+          var urlOFF = data._links.channel;
+          $http.get(urlOFF).success(function(dataOFF){
+          $scope.offline.push(dataOFF);  
+          });//getURLOFF
+        }      
+    }).error(function(data,status) {//when account user is closed    
+      //create an object with the data 
+      var chOut = {
+        stream:null,
+        logo:null,
+        display_name:user,
+        url:"https://www.twitch.tv/"+user,
+        status_account:"Account Closed"
+      };
+        //push closed account
+        $scope.offline.push(chOut);      
+      });//end error
+  });//end foreach  
+});
